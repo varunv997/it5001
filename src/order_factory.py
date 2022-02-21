4eeb0d1a947ede49cf744d3c380b2e3379647418
Varun Narayanan <varunv997@outlook.com>
2022-02-21 12:11:14 +0800

    Remove all order implementations.

--- src/order_factory.py
+++ src/order_factory.py
@@ -1,6 +1,6 @@
 """file defines OrderFactory"""
 
-from order import LimitOrder, MarketOrder, IOCOrder, FOKOrder, GTCOrder
+from order import Order
 
 
 class OrderFactory:
@@ -17,11 +17,6 @@ class OrderFactory:
     """
 
     submit_orders = {
-        "LO": LimitOrder,
-        "MO": MarketOrder,
-        "IOC": IOCOrder,
-        "FOK": FOKOrder,
-        "GTC": GTCOrder,
     }
 
     @classmethod
